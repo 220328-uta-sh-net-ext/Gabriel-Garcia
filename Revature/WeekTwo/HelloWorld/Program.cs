9799a89978ae6218494d570b95e2e0e08258d690
GabrielPGarciaJr <98702411+GabrielPGarciaJr@users.noreply.github.com>
2022-04-06 14:25:03 -0500

    fixing up my files

--- Revature/WeekTwo/HelloWorld/Program.cs
+++ Revature/WeekTwo/HelloWorld/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿//using System;
 
 namespace HelloWorld
 {
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         public static void Main()
         {
-            Console.WriteLine("Hello World!")
+            Console.WriteLine("Hello World!");
         }
     }
 }

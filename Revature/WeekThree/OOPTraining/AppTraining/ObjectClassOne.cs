5f12ef13d0cd2b99bed26b420d2d49a2a63ba9b0
GabrielPGarciaJr <98702411+GabrielPGarciaJr@users.noreply.github.com>
2022-04-16 05:01:57 -0500

    Mass changes

---
+++ Revature/WeekThree/OOPTraining/AppTraining/ObjectClassOne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//creating a new class 
+namespace AppTraining
+{
+    internal class ObjectClassOne
+    {
+        public string sName = "name";//setting up some variables 
+        public const string csName = "csName";//can't be changed
+
+        //mwthods
+        public void TestDoThings(string sString,string sName)
+        {
+            //this.sName is pointing to the one in ObjectClassOne while the sName is pointing to the variable located in TestDoThings
+            this.sName = sName;
+            Console.WriteLine($"{sString} and {sName}");
+        }
+    }
+}
---
+++ Revature/WeekThree/OOPTraining/AppTraining/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using AppTraining;
+
+ObjectClassOne nObj = new ObjectClassOne();
+nObj.sName = "Test";
+string sNewName = nObj.sName;//setting local variable to ObjectClassOne's sName variable
+Console.WriteLine(sNewName);//print the local variable
+Console.WriteLine(ObjectClassOne.csName);//printing the constent
+nObj.TestDoThings("Test 2 meow", "Test 3");//method
